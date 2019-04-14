05696d5440b5446603b906a81e857b20374b7b7b
Chris Murphy <chris_r_murphy@hotmail.com>
2019-04-13 21:55:16 -0400

    Leave it blank

--- java/AccessDatabase.java
+++ java/AccessDatabase.java
@@ -501,9 +501,6 @@ public class AccessDatabase {
                 }
             }
             String[] parsedquery=query.split(",");
-            for(String s:parsedquery){
-                System.out.println(s);
-            }
             for(int i=0; i<parsedquery.length; i++){
                 if(parsedquery[i].contains("OPTIONAL_UPGRADES.OPTIONAL_UPGRADE")){
                     optUpgrade=i;
